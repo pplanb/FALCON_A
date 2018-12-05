0562382521709d6979562634535f38250eb891ac
pplanb <rolandhablutzel@hotmail.com>
2018-12-05 21:43:39 +0100

    update falcon a

--- _GetDATToProd.bat
+++ _GetDATToProd.bat
@@ -5,11 +5,13 @@ rem To be used after training of the robot using Strategy Tester
 setlocal enabledelayedexpansion
 
 :: files generated in the tester\files folder
-set SOURCE_DIR="C:\Program Files (x86)\FxPro - Terminal2\tester\files"
+set SOURCE_DIR="D:\FxPro - Terminal3\tester\files"
 :: files copied to the sandbox folder MQL4\Files
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Files"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Files"
+set DEST_DIR1="D:\FxPro - Terminal1\MQL4\Files"
+set DEST_DIR2="D:\FxPro - Terminal3\MQL4\Files"
 
 rem only copy *.dat files
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.dat
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.dat
+
+pause
